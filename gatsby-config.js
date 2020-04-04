500bdc44ac60ffd5c94a44bcb0865c0f7452cd63
Михаил Колосков <meeq@yamoney.ru>
2020-04-05 01:36:26 +0300

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,15 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        custom: {
+          families: ["Spectral, Arial"],
+          urls: ["/src/fonts/fonts.css"],
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
